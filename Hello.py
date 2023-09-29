e16678f9791a4bb4c3aa1776145b8211055e099f
Brad Yoo <byoo@janison.com>
2023-09-29 22:22:33 +0900

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import yfinance as yf
 import streamlit as st
 from streamlit.logger import get_logger
 
@@ -19,33 +19,25 @@ LOGGER = get_logger(__name__)
 
 
 def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
+    # https://www.markdownguide.org/cheat-sheet/
+    st.write("""
+    ### Simple Stock Price App
+    
+    Shown are the stock **closing price** and ***volume*** of Google!
+    
+    """)
+    
+    # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+    #define the ticker symbol
+    tickerSymbol = 'GOOGL'
+    #get data on this ticker
+    tickerData = yf.Ticker(tickerSymbol)
+    #get the historical prices for this ticker
+    tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+    # Open	High	Low	Close	Volume	Dividends	Stock Splits
+    
+    st.line_chart(tickerDf.Close)
+    st.line_chart(tickerDf.Volume)
 
 if __name__ == "__main__":
     run()
